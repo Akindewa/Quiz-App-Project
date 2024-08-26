74586fb4a35801a67f245ad4e141e34a28f86697
Akindewa <raodhotakindewa@gmail.com>
2024-08-26 15:18:30 +0100

    extra space causing confusion

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 let questions = [
     {
     numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
+    question: "What is the output of typeof undefined?",
+    answer: "undefined",
     options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
+      "object",
+      "null",
+      "undefined",
+      "boolean"
     ]
   },
     {
     numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
+    question: "Which method converts a JSON string into a JavaScript object?",
+    answer: "JSON.parse()",
     options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
+      "JSON.stringify()",
+      "JSON.objectify()",
+      "JSON.parse()",
+      "JSON.toObject()"
     ]
   },
     {
     numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
+    question: "Which of the following is not a JavaScript data type?",
+    answer: "Character",
     options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
+      "Character",
+      "Boolean",
+      "String",
+      "Number"
     ]
   },
     {
@@ -54,18 +54,546 @@ let questions = [
       "eXamine Multiple Language"
     ]
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+  {
+    numb: 6,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 13,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 17,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 20,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 21,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 25,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 26,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 27,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 28,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 29,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 30,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 31,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 32,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 33,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 34,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 35,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 36,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 37,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 38,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 39,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 40,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 41,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 42,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 43,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 44,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 45,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 46,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 47,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 48,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 49,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  },
+
+  {
+    numb: 50,
+    question: "What does === do in JavaScript?",
+    answer: "Compares values and types",
+    options: [
+      "Compares only values",
+      "Compares values and types",
+      "Assigns a value",
+      "Check out for value only"
+    ]
+  }
+
+  
 ];
